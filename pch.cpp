f4b16f50c34bad08d6e56709e4ea0e2a2c9088da
黑米粥工作室 <62678724+Heymmy-Zhou@users.noreply.github.com>
2020-04-15 17:43:08 +0800

    Add files via upload

---
+++ pch.cpp
@@ -0,0 +1,17 @@
+﻿/*
+Copyright(c) 2020 黑米粥工作室
+黑米粥杀毒 V2.0 is licensed under Mulan PSL v2.
+You can use this software according to the termsand conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at :
+http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON - INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+*/
+
+// pch.cpp: 与预编译标头对应的源文件
+
+#include "pch.h"
+
+// 当使用预编译的头时，需要使用此源文件，编译才能成功。
---
+++ targetver.h
@@ -0,0 +1,20 @@
+﻿/*
+Copyright(c) 2020 黑米粥工作室
+黑米粥杀毒 V2.0 is licensed under Mulan PSL v2.
+You can use this software according to the termsand conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at :
+http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON - INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+*/
+
+#pragma once
+
+// 包括 SDKDDKVer.h 将定义可用的最高版本的 Windows 平台。
+
+// 如果要为以前的 Windows 平台生成应用程序，请包括 WinSDKVer.h，并将
+// 将 _WIN32_WINNT 宏设置为要支持的平台，然后再包括 SDKDDKVer.h。
+
+#include <SDKDDKVer.h>
