2f38ddfc53de92b3017230b8e48428ae2b4fa4a2
黑米粥工作室 <62678724+Heymmy-Zhou@users.noreply.github.com>
2020-04-17 11:07:11 +0800

    Add files via upload

---
+++ Heymmy-Zhou AntivirusDlg.h
@@ -0,0 +1,67 @@
+﻿/*
+Copyright(c) 2020 黑米粥工作室
+黑米粥杀毒 V2.01 is licensed under Mulan PSL v2.
+You can use this software according to the termsand conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at :
+http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON - INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+*/
+
+// Heymmy-Zhou AntivirusDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CHeymmyZhouAntivirusDlg 对话框
+class CHeymmyZhouAntivirusDlg : public CDialogEx
+{
+// 构造
+public:
+	CHeymmyZhouAntivirusDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_HEYMMYZHOUANTIVIRUS_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedAbout();
+	afx_msg void OnBnClickedWinver();
+	afx_msg void OnBnClickedSd();
+	afx_msg void OnBnClickedCtrl();
+	afx_msg void OnBnClickedButton11();
+	afx_msg void OnBnClickedLaji();
+	afx_msg void OnBnClickedInf();
+	afx_msg void OnBnClickedReg();
+	afx_msg void OnBnClickedFang();
+	afx_msg void OnBnClickedTodo();
+	afx_msg void OnBnClickedComp();
+	afx_msg void OnBnClickedShij();
+	afx_msg void OnBnClickedXn();
+	afx_msg void OnBnClickedZy();
+	afx_msg void OnBnClickedZj();
+	afx_msg void OnBnClickedQd();
+	afx_msg void OnBnClickedAq();
+	afx_msg void OnBnClickedFw();
+	afx_msg void OnBnClickedPrint();
+	afx_msg void OnBnClickedHf();
+	afx_msg void OnBnClickedTsk();
+};
---
+++ resource.h
@@ -0,0 +1,40 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 HeymmyZhouAntivirus.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_HEYMMYZHOUANTIVIRUS_DIALOG  102
+#define IDR_MAINFRAME                   128
+#define IDC_LICENSE                     1000
+#define IDC_ABOUT                       1001
+#define IDC_LAJI                        1002
+#define IDC_REG                         1003
+#define IDC_WINVER                      1008
+#define IDC_SD                          1009
+#define IDC_CTRL                        1011
+#define IDC_INF                         1013
+#define IDC_FANG                        1014
+#define IDC_TODO                        1015
+#define IDC_COMP                        1016
+#define IDC_SHIJ                        1017
+#define IDC_XN                          1018
+#define IDC_ZJ                          1019
+#define IDC_FW                          1022
+#define IDC_CAP                         1027
+#define IDC_TSK                         1028
+#define IDC_BUTTON26                    1029
+#define IDC_BUTTON2                     1031
+#define IDC_INTER                       1031
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1032
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
